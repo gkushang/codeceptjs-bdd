b68f891e9821c5b3c81e6c651b15315dd613f9df
Kushang Gajjar <g.kushang@gmail.com>
2019-08-12 17:53:26 -0400

    CodeceptJS CLI

--- packages/codeceptjs-cucumber/cli.js
+++ packages/codeceptjs-cucumber/cli.js
@@ -1,44 +1,60 @@
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const figlet = require("figlet");
-const shell = require("shelljs");
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const shell = require('shelljs');
+const emoji = require('node-emoji');
 
 const init = () => {
-    console.log(
-        chalk.green(
-            figlet.textSync("CodeceptJS E2E Framework", {
-                font: "Ghost",
-                horizontalLayout: "default",
-                verticalLayout: "default"
+    console.log('\n' + 
+        chalk.yellow(
+            figlet.textSync('E2E Codecept JS', {
+                font: 'Ghost',
+                horizontalLayout: 'default',
+                verticalLayout: 'default'
             })
         )
     );
+
+    console.log(chalk.blue.underline.bold(
+        '\nWelcome to CodeceptJs-E2E CLI!'
+    ))
+
+    console.log(
+        chalk.green.bold(
+        '\nThe CodeceptJs-E2E CLI is a command-line interface tool that you use to Create and Initialize the base ' +
+            chalk.blue.bold.underline('CodeceptJS Cucumber E2E Frameowork') +
+            ' to your workspace. It will create ' + chalk.blue.bold.underline('acceptance') + ' directory at your acceptance test location and ' + chalk.blue.bold.underline('configurations') + ' under your workspace.\n'));
 }
 
-const askQuestions = () => {
+const askInitQuestions = () => {
     const questions = [
         {
-            name: "DESIRED_PROJECT_PATH",
-            type: "input",
-            message: "Enter the path to your desired project: "
+            name: 'DESIRED_PROJECT_PATH',
+            type: 'input',
+            message: 'Enter the path to your destination Root project: '
         },
         {
-            name: "ACCEPTANCE_PATH",
-            type: "input",
-            message: "Enter the path to colocate the Acceptance tests: "
+            name: 'ACCEPTANCE_PATH',
+            type: 'input',
+            message: 'Enter the path to your desitnation Acceptance tests location: '
+        },
+        {
+            type: 'confirm',
+            name: 'RUN',
+            message: 'The CLI also offers to test your setup and configurations. It provides the simple example tests of GitHub Search Feature. Do you want to run a sample GitHub Tests once your Setup is complete?'
         }
+
 
     ];
     return inquirer.prompt(questions);
 };
 
-const askQuestionsToRun = () => {
+const askRunQuestions = () => {
     const questions = [
         {
-            type: "list",
-            name: "RUN",
-            message: "The setup comes with examples of GitHub Acceptance Tests. Do you want to run a sample GitHub Tests to check your setup?",
-            choices: ["Yes", "No"]
+            type: 'confirm',
+            name: 'RUN',
+            message: 'The setup comes with examples of GitHub Acceptance Tests. Do you want to run a sample GitHub Tests to check your setup?'
         }
 
     ];
@@ -46,45 +62,40 @@ const askQuestionsToRun = () => {
 };
 
 const success = (filepath) => {
-    console.log(
-        chalk.white.bgGreen.bold(`Done! Acceptance Tests Created at ${filepath}`)
+    console.log('\n' +
+        chalk.white.bgRed.bold(emoji.emojify(':clap: :thumbsup:') + ` Done! Acceptance Tests Created at ${filepath}\n`)
     );
 };
 
+const failure = (message) => {
+    console.log('\n' +
+        chalk.bold.red(emoji.emojify(':warning:  ') + emoji.emojify(':disappointed: ') + 'Error: ' + message)
+    );
+    shell.exit(1);
+};
 
 const run = async () => {
-    // show script introduction
     init();
 
-    // ask questions
-    // ask questions
-    const answers = await askQuestions();
-    const { DESIRED_PROJECT_PATH, ACCEPTANCE_PATH } = answers;
+    const answers = await askInitQuestions();
 
-    console.log('path: ', ACCEPTANCE_PATH);
+    const { DESIRED_PROJECT_PATH, ACCEPTANCE_PATH, RUN } = answers;
+
     shell.cp('-R', 'packages/codeceptjs-cucumber/acceptance', ACCEPTANCE_PATH);
     shell.cp('-R', 'packages/codeceptjs-cucumber/codecept.conf.js', ACCEPTANCE_PATH);
-
-    console.log('cd: ', DESIRED_PROJECT_PATH);
     shell.cd(DESIRED_PROJECT_PATH);
 
     if (shell.exec('yarn add codeceptjs-saucelabs codeceptjs-shared @wdio/selenium-standalone-service allure-commandline codeceptjs codeceptjs-selenium debug faker protractor rimraf should webdriverio deepmerge -D' ).code !== 0) {
-        shell.echo('Error: Yarn Install failed');
-        shell.exit(1);
+        failure('Yarn command failed.');
     }
 
-    const run = await askQuestionsToRun();
-    const { RUN } = run;
-
-    if(RUN === 'Yes') {
+    if(RUN === true) {
         if (shell.exec('./node_modules/.bin/codeceptjs run --config=./modules/codecept.conf.js --grep=@search_results' ).code !== 0) {
-            shell.echo('Error: Execution failed');
-            shell.exit(1);
+            failure('Execution of Acceptance Test Failed.');
         }
     }
 
     success(ACCEPTANCE_PATH);
-    shell.exit(1);
 };
 
 run();
