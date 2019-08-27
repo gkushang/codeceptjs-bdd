ff1af48eddf884505e3f449724c8413f93c5b3d3
Kushang Gajjar <g.kushang@gmail.com>
2019-08-27 10:16:27 -0400

    remove additional files

--- acceptance/.secrets.js
+++
@@ -1,7 +0,0 @@
-console.log('************************************************************');
-console.log('*                     EXPORTING VARIABLES                  *');
-console.log('************************************************************');
-
-process.env.SAUCE_USERNAME='<sauce_username>';
-process.env.SAUCE_KEY='<sauce_key>';
-
