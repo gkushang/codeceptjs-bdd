8baa7a097f63e6285f8cd40982e21acf9ca0502e
Shikhar Prasoon <sprasoon@salesforce.com>
2020-04-23 15:21:58 -0400

    reduce config file

--- packages/create-codeceptjs-bdd-tests/codecept.conf.js
+++ packages/create-codeceptjs-bdd-tests/codecept.conf.js
@@ -1,42 +1,12 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-// update to 'webdriver' or 'playwright' for the selective browser
-process.env.DRIVER = process.env.DRIVER || 'webdriver';
-process.env.DEFAULT_WEBDRIVER_BROWSER = 'chrome';
-process.env.DEFAULT_PLAYWRIGHT_BROWSER = 'chromium';
-
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './acceptance/config/dev.codecept.env',
+});
 const configure = require('codeceptjs-shared').configure;
 
-const DEFAULT_HOST = 'github.com';
-const RELATIVE_PATH = './tests/acceptance/';
-const PAGES_PATH = RELATIVE_PATH + 'pages/';
-
-const HOST = configure.buildHost(DEFAULT_HOST);
-
 let conf = {
-    output: RELATIVE_PATH + 'report',
-    cleanup: true,
-    dev: {
-        host: HOST,
-    },
-    helpers: {
-        Playwright: {
-            url: HOST,
-        },
-        WebDriver: {
-            url: HOST,
-        },
-        REST: {},
-    },
-    gherkin: {
-        features: RELATIVE_PATH + 'features/**/*.feature',
-    },
-    include: {
-        I: RELATIVE_PATH + 'helpers/custom.methods.js',
-        ghHomePage: PAGES_PATH + 'github/gh-home.page.js',
-        ghSearchPage: PAGES_PATH + 'github/gh-search.page.js',
-    },
-    tests: RELATIVE_PATH + 'mocha/*.test.js',
     name: 'Codeceptjs BDD Acceptance Tests',
 };
 
+console.log(configure.create(conf));
 exports.config = configure.create(conf);
